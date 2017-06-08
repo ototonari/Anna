b59d443287319d28ae2e3e244f3b69a5bdb65c7b
ototonari <sinnyo_tsubasa@icloud.com>
2017-06-09 00:40:34 +0900

    ファイル送信に成功しなかった場合、３回までトライする。その後にローカルファイルを削除する

--- smartFileTransfer.sh
+++ smartFileTransfer.sh
@@ -1,28 +1,47 @@
 #!/bin/bash
 
-
+# def var
 FILE=$1
+IPADDRESS=$(cat ipaddress.txt)
+# transfer file path
+FILEPATH="file/GD-${FILE}.mp3"
+
+# def func
+
+file_transfer ()
+{
+	local filepath=$1
+	expect -c "
+		set timeout -1
+		spawn env LANG=C /usr/bin/sftp pi@${IPADDRESS}
+		expect \"sftp>\ \"
+		send \"put ${filepath}\n\"
+		expect \"sftp>\ \"
+		send \"bye\n\"
+		exit 0
+	"
+
+}
 
 # .wav conversion .mp3 and gain
 sox file/${FILE}.wav file/${FILE}.mp3 
 sox file/${FILE}.mp3 file/tmp${FILE}.mp3 gain -n
 sox file/tmp${FILE}.mp3 file/GD-${FILE}.mp3 gain -l 5
 
-# transfer file path
-FILEPATH="file/GD-${FILE}.mp3"
+# transfer
+#file_transfer "$FILEPATH"
 
-# tmp remove
-rm file/tmp${FILE}.mp3 &
+i=3
+while [ "$i" -ge 1 ]
+do
+	if file_transfer "$FILEPATH"; then
+		echo "transfer DONE. ${i}"
+		i=0
+	else
+		echo "transfer Failure."
+		i=$((i - 1))
+	fi
+done
 
-# transfer File
-IPADDRESS=$(cat ipaddress.txt)
-echo "$FILEPATH"
-expect -c "
-        set timeout -1
-        spawn env LANG=C /usr/bin/sftp pi@${IPADDRESS}
-        expect \"sftp>\ \"
-        send \"put ${FILEPATH}\n\"
-        expect \"sftp>\ \"
-        send \"bye\n\"
-        exit 0
-"
+# local .wav .mp3 remove
+rm file/tmp${FILE}.mp3 file/${FILE}.wav file/GD-${FILE}.mp3 file/${FILE}.mp3
