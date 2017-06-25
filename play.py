33c0da53b4cc753abd43380758abdc2f55478815
TsubasaNagata <tsubasa@ST-MAC.local>
2017-06-25 18:03:41 +0900

    no message

--- play.py
+++ play.py
@@ -8,6 +8,5 @@ import sys
 f = open("playedList", 'a', encoding="utf-8")
 for file in playList():
    print(file) # 再生したとみなす
-   #f.write("{}".format(file)) # 改行追加して記述
-   f.writelines(file)
+   f.write("{},".format(file)) # 改行追加して記述
 f.close
