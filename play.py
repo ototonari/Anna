8f11c7711fe0af5a55fd125e8ae0dbc7423be89c
ototonari <sinnyo_tsubasa@icloud.com>
2017-06-25 19:44:50 +0900

    fix play.py

--- play.py
+++ play.py
@@ -8,9 +8,9 @@ from time import sleep
 
 while True:
    f = open("playedList", 'a', encoding="utf-8")
-      for file in playList():
-         os.system("play {}".format(file)) # ファイル再生
-         f.write("{},".format(file)) # 改行追加して記述
+   for file in playList():
+      os.system("play {}".format(file)) # ファイル再生
+      f.write("{},".format(file)) # 改行追加して記述
    f.close
    sleep(60)
    
