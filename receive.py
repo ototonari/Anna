3f45100c1bb11754e7e47f7e979f6025d1225e6c
Tsubasa <sinnyo_tsubasa@icloud.com>
2017-08-04 17:21:11 +0900

    new receive

---
+++ receive.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from datetime import datetime, timedelta, time
+from play import Player
+from time import sleep
+
+now = datetime.now()
+try:
+    while now.hour <= 17:
+        p = Player()
+        p.checkFilelist()
+        p.pickUpLog()
+        p.extraction()
+        p.sort()
+        p.download()
+        p.play()
+        p.delete()
+        sleep(60)
+        now = datetime.now()
+
+except:
+    raise ValueError(" is Failure.")
