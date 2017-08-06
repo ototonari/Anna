f721821ab813094c8f7b3596a8c50b5956df5210
ototonari <sinnyo_tsubasa@icloud.com>
2017-08-06 08:35:37 +0000

    ver4.0

--- manager.py
+++ manager.py
@@ -14,17 +14,35 @@ def checkProcessId(script, file):
         print("process is nothing.")
         return 0
 
-def generateProcess(script, file):
-    cmd = "{} {}".format(script, file)
-    args = shlex.split(cmd)
-    p = subprocess.Popen(args)
-    print("generateProcess to {cmd}".format(cmd=cmd))
+def generateProcess(args):
+    try:
+        if args:
+            cmd = ""
+            for line in args:
+                cmd += "{} ".format(line)
+            args = shlex.split(cmd)
+            p = subprocess.Popen(args)
+            print("generateProcess to {cmd}".format(cmd=cmd))
+        else:
+            print("don't generateProcess. no argument.")
+
+    except:
+        print(sys.exc_info()[0])
+        traceback.print_exc(file=sys.stdout)
+        pass
+
 
 def killProcess(processId):
     cmd = "kill {}".format(processId)
     args = shlex.split(cmd)
     p = subprocess.Popen(args)
     print("killProcess to pid {pid}".format(pid=processId))
+
+def familyKilling(parentName):
+    cmd = "bash ./killpstree.bash {}".format(parentName)
+    args = shlex.split(cmd)
+    p = subprocess.Popen(args)
+    print("Killing {}'s family".format(parentName))
 
 def shellCommand(commands):
     args = shlex.split(commands)
@@ -36,15 +54,19 @@ try:
     if __name__ == '__main__':
         argvs = sys.argv
         order = argvs[1]
-        args = (argvs[2], argvs[3])
+        args = argvs[2:]
         if order == "generate":
             if checkProcessId(args[0], args[1]) == 0:
-                generateProcess(args[0], args[1])
+                generateProcess(args)
 
         elif order == "kill":
             processId = checkProcessId(args[0], args[1])
             if processId:
                 killProcess(processId)
+
+        elif order == "killing":
+            if args[1]:
+                familyKilling(args[1])
 
 except IndexError:
     print(sys.exc_info()[0])
